3453352f8d77f76e152b8ba532f24cb3657a9257
AdamDecloitre <decloitreadam@gmail.com>
2026-02-11 14:10:38 +0100

    envoie tp flask perso

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return render_template("index.html")
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+    return render_template("about.html")
 
-@app.route('/about')
-def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+@app.route("/cv")
+def cv():
+    return render_template("cv.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
